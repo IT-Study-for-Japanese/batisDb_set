a62b2e454a9db5a389332a1da6d895e3c87db485
Bik00 <bikoo3002@naver.com>
2023-08-25 16:43:24 +0900

    reserveHome.jsp 파일 내 코드 분할

--- src/main/webapp/js/rent.js
+++ src/main/webapp/js/rent.js
@@ -24,18 +24,37 @@ $(document).ready(function () {
         });
     });
 
-    function updateTable(data) {
+    $(".reserve-button").click(function () {
+        // 팝업 열기
+        $("#myModal").css("display", "block");
+    });
 
-        const tbody = $("table tbody"); //table요소 <tbody>선택
-        tbody.empty();
-        
-        $.each(data, function (index, rent) {
-                var row = $("<tr>");
-                row.append($("<td>").text(rent.reservePlaceName));
-                row.append($("<td>").text(rent.reservePlaceAddr));
-                row.append($("<td>").text("카운트 구현필요"));
-                tbody.append(row);
-            }
-        );
-    }
-}); 
+    // 모달 팝업 닫기
+    $(".close").click(function () {
+        // 팝업 닫기
+        $("#myModal").css("display", "none");
+    });
+
+    // 모달 외부 클릭 시 팝업 닫기
+    $(window).click(function (e) {
+        if (e.target == $("#myModal")[0]) {
+            // 팝업 닫기
+            $("#myModal").css("display", "none");
+        }
+    });
+});
+
+function updateTable(data) {
+
+    const tbody = $("table tbody"); //table요소 <tbody>선택
+    tbody.empty();
+
+    $.each(data, function (index, rent) {
+            var row = $("<tr>");
+            row.append($("<td>").text(rent.reservePlaceName));
+            row.append($("<td>").text(rent.reservePlaceAddr));
+            row.append($("<td>").text("카운트 구현필요"));
+            tbody.append(row);
+        }
+    );
+}
